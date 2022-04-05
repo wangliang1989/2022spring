c79f822b8774d38f2c84b939959357c303eec50d
王亮 <wangliang0222@gmail.com>
2022-04-05 13:11:13 +0800

    script to output pdfs

---
+++ pdf/print.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+foreach (glob "../homework/*_homework.csv") {
+    my ($name) = (split m/\//)[-1];
+    ($name) = split m/\./, $name;
+    system "cupsfilter $_ > $name.pdf";
+}
+system "cupsfilter ../rollcall/kaoqing.txt > 考勤.pdf";
