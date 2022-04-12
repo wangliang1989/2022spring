3f344ca4d3765b24e8ead59b55536c7abc20c278
wangliang <wangliang0222@gmail.com>
2022-04-12 20:25:24 +0800

    fix

--- pdf/print.pl
+++ pdf/print.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-foreach (glob "../homework/*_homework.csv") {
+foreach (glob "../homework/*_out.csv") {
     my ($name) = (split m/\//)[-1];
     ($name) = split m/\./, $name;
     system "cupsfilter $_ > $name.pdf";
