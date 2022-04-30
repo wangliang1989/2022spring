a7d6966b308687fc556ad132721caf2f4e3e428f
王亮 <wangliang0222@gmail.com>
2022-04-30 17:19:06 +0800

    打印文件

--- pdf/print.pl
+++ pdf/print.pl
@@ -2,9 +2,14 @@
 use strict;
 use warnings;
 
-foreach (glob "../homework/*_out.csv") {
+foreach (glob "../homework/作业成绩*") {
     my ($name) = (split m/\//)[-1];
     ($name) = split m/\./, $name;
     system "cupsfilter $_ > $name.pdf";
 }
 system "cupsfilter ../rollcall/kaoqing.txt > 考勤.pdf";
+foreach (glob "../performance/课堂表现*") {
+    my ($name) = (split m/\//)[-1];
+    ($name) = split m/\./, $name;
+    system "cupsfilter $_ > $name.pdf";
+}
