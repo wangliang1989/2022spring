00399fbba430eed11dc4f014f292ab083b3b410d
王亮 <wangliang0222@gmail.com>
2022-05-08 23:45:53 +0800

    重新录入时间成绩

--- performance/perfor2.pl
+++ performance/perfor2.pl
@@ -18,30 +18,47 @@ foreach my $file (glob "../list/*_official.csv") {
     my %top_line;
     ($medians{$_}, $top_line{$_}) = get_th($_, keys %students) for (3, 4, 5, 6, 7, 8, 9);
     my %records;
-    foreach my $file (glob "jg.*.txt") {
-        open (IN, "< $file") or die;
-        foreach (<IN>) {
-            #刘仙华 2130612001 4 1653002380 95
-            my ($name, $id, $num, $time, undef) = split m/\s+/;
-            next unless grep{$_ eq $id} keys %students;
-            next unless $num > 2 and $num < 10;
-            die $num unless defined ($medians{$num});
-            my $score = int(85 + ($medians{$num} - $time) / 43200 + 0.5);
-            $score = 95 if $time <= $top_line{$num} and $score < 95;
-            $score = 100 if $score > 100;
-            if (defined($records{$id})) {
-                $records{$id} = "$records{$id} $score";
-            }else{
-                $records{$id} = $score;
+    open (IN, "< time/old.csv") or die;
+    foreach (<IN>) {
+        #刘仙华 2130612001 4 1653002380 95
+        my ($name, $id, $num, $origin, undef) = split m/\s+/;
+        next unless grep{$_ eq $id} keys %students;
+        next unless $num > 2 and $num < 10;
+        die $num unless defined ($medians{$num});
+        my ($time) = origin2sec($origin);
+        my $score = int(85 + ($medians{$num} - $time) / 43200 + 0.5);
+        $score = 95 if $time <= $top_line{$num} and $score < 95;
+        $score = 100 if $score > 100;
+        if (defined($records{"$id $num"})) {
+            $records{"$id $num"} = $score if $records{"$id $num"} < $score;
+        }else{
+            $records{"$id $num"} = $score;
+        }
+    }
+    close(IN);
+    open (OUT, "> perfor2_$class.txt") or die;
+    foreach my $id (sort {$a <=> $b} keys %students) {
+        print OUT "$students{$id}";
+        foreach my $num (3, 4, 5, 6, 7, 8, 9) {
+            foreach (keys %records) {
+                print OUT " $records{$_}" if ($_ eq "$id $num")
             }
         }
-        close(IN);
-    }
-    open (OUT, "> perfor2_$class.txt") or die;
-    foreach (sort {$a <=> $b} keys %students) {
-        print OUT "$students{$_} $records{$_}\n" if defined($records{$_});
+        print OUT "\n";
     }
     close(OUT);
+}
+sub origin2sec {
+    use Time::Local;
+    my @out;
+    foreach (@_) {
+        my ($date, $time) = split "T";
+        my ($year, $mon, $day) = split "-", $date;
+        my ($hour, $min, $sec) = split ":", $time;
+        my $t = timegm($sec, $min, $hour, $day, $mon, $year);
+        push @out, $t;
+    }
+    return @out;
 }
 sub get_th{
     my ($i, @students) = @_;
